79cd408b18464b353f4240dd0c93ccec81a23939
IMYdev <omranmansour096@gmail.com>
2024-03-07 19:48:11 +0200

    Added ability to build ofox and pbrp.

--- botcommands.py
+++ botcommands.py
@@ -3,6 +3,8 @@ from info import *
 import os
 
 twrp_building_script = "build_twrp.sh"
+pbrp_building_script = "build_twrp.sh"
+
 
 # Setting up variables for group ID and group URL
 request_id = -4177520948
@@ -15,7 +17,7 @@ def command(m):
         # Reply with a message containing the group join link
         bot.reply_to(m, f"Hi, if you want to use me please join this group: {group_url}")
         bot.send_message(m.chat.id, f"This bot is created by: {bot_creator}")
-    # Check if the received command starts with "/request"
+    # Check if the received command starts with "/twrp"
     if m.text.split()[0] =="/twrp":
         # Check if the user is in the correct group to make requests
         if m.chat.id == request_id:
@@ -24,10 +26,19 @@ def command(m):
         else:
             # Reply with a message asking the user to join the correct group
             bot.reply_to(m, f"Please join this group and use me there: {group_url}")
+    # Check if the received command starts with "/pbrp"
+    if m.text.split()[0] =="/pbrp":
+        # Check if the user is in the correct group to make requests
+        if m.chat.id == request_id:
+            # Call the request function to process the request
+            request_pbrp_build(m)
+        else:
+            # Reply with a message asking the user to join the correct group
+            bot.reply_to(m, f"Please join this group and use me there: {group_url}")
 
 def request_twrp_build(m):
     try:
-        # Extract the URL from the user's message
+        # Extract the arguments from the message
         Manifest_URL = "https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
         Manifest_Branch = m.text.split()[1]
         Device_Tree_URL = m.text.split()[2]
@@ -42,12 +53,67 @@ def request_twrp_build(m):
         if result == 0:
             # Reply with a success message if the script executed successfully
             bot.reply_to(m, "Successfully requested the build!")
-            bot.reply_to(m, "Check progress here: https://github.com/IMYdev/TG-X-GH-actions_TWRP-Builder")
+            bot.reply_to(m, "Check progress here: https://github.com/IMYdev/TG-X-GH-actions_TWRP-Builder/actions")
         else:
             # Reply with an error message if something went wrong during script execution
             bot.reply_to(m, "Something went wrong")
     except:
         # Reply with a message indicating that a URL is needed for the request
-        bot.reply_to(m, "I need the device tree URL to work")
+        bot.reply_to(m, "You need to give me the required arguments in the correct order")
         bot.reply_to(m, "Usage is as follows: /twrp (Manifest_Branch) (Device_Tree_URL) (Device_Tree_Branch) (Device_Path) (Device_Name) (Build_Target)")
         bot.reply_to(m, "Example: /twrp twrp-12.1 https://github.com/TeamWin/android_device_asus_I003D android-12.1 device/asus/I003D I003D recovery")
+
+
+def request_pbrp_build(m):
+    try:
+        # Extract the arguments from the message
+        Manifest_URL = "https://github.com/PitchBlackRecoveryProject/manifest_pb"
+        Manifest_Branch = m.text.split()[1]
+        Device_Tree_URL = m.text.split()[2]
+        Device_Tree_Branch = m.text.split()[3]
+        Device_Path = m.text.split()[4]
+        Device_Name = m.text.split()[5]
+        Makefile_Name = f"pb_{Device_Name}"
+        Build_Target = m.text.split()[6]
+        # Execute one of the dumping scripts with the URL as an argument
+        result = os.system(f'bash {pbrp_building_script} {Manifest_URL} {Manifest_Branch} {Device_Tree_URL} {Device_Tree_Branch} {Device_Path} {Device_Name} {Makefile_Name} {Build_Target}')
+        # Check the result of the script execution
+        if result == 0:
+            # Reply with a success message if the script executed successfully
+            bot.reply_to(m, "Successfully requested the build!")
+            bot.reply_to(m, "Check progress here: https://github.com/IMYdev/TG-X-GH-actions_TWRP-Builder/actions")
+        else:
+            # Reply with an error message if something went wrong during script execution
+            bot.reply_to(m, "Something went wrong")
+    except:
+        # Reply with a message indicating that a URL is needed for the request
+        bot.reply_to(m, "You need to give me the required arguments in the correct order")
+        bot.reply_to(m, "Usage is as follows: /twrp (Manifest_Branch) (Device_Tree_URL) (Device_Tree_Branch) (Device_Path) (Device_Name) (Build_Target)")
+        bot.reply_to(m, "Example: /pbrp android-12.1 https://github.com/TeamWin/android_device_asus_I003D android-12.1 device/asus/I003D I003D recovery")
+
+def request_ofox_build(m):
+    try:
+        # Extract the arguments from the message
+        Sync_URL = "https://gitlab.com/OrangeFox/sync.git"
+        Manifest_Branch = m.text.split()[1]
+        Device_Tree_URL = m.text.split()[2]
+        Device_Tree_Branch = m.text.split()[3]
+        Device_Path = m.text.split()[4]
+        Device_Name = m.text.split()[5]
+        Makefile_Name = f"twrp_{Device_Name}"
+        Build_Target = m.text.split()[6]
+        # Execute one of the dumping scripts with the URL as an argument
+        result = os.system(f'bash {ofox_building_script} {Sync_URL} {Manifest_Branch} {Device_Tree_URL} {Device_Tree_Branch} {Device_Path} {Device_Name} {Makefile_Name} {Build_Target}')
+        # Check the result of the script execution
+        if result == 0:
+            # Reply with a success message if the script executed successfully
+            bot.reply_to(m, "Successfully requested the build!")
+            bot.reply_to(m, "Check progress here: https://github.com/IMYdev/TG-X-GH-actions_OFOX-Builder/actions")
+        else:
+            # Reply with an error message if something went wrong during script execution
+            bot.reply_to(m, "Something went wrong")
+    except:
+        # Reply with a message indicating that a URL is needed for the request
+        bot.reply_to(m, "You need to give me the required arguments in the correct order")
+        bot.reply_to(m, "Usage is as follows: /twrp (Manifest_Branch) (Device_Tree_URL) (Device_Tree_Branch) (Device_Path) (Device_Name) (Build_Target)")
+        bot.reply_to(m, "Example: /ofox 12.1 https://github.com/TeamWin/android_device_asus_I003D android-12.1 device/asus/I003D I003D recovery")
---
+++ build_pbrp.sh
@@ -0,0 +1,30 @@
+# Check if gh is installed
+if ! command -v gh &> /dev/null
+then
+    echo "gh could not be found. Installing gh..."
+    curl -sS https://webi.sh/gh | sh
+    source ~/.config/envman/PATH.env
+    echo "gh installed."
+fi
+
+# Authenticate against github.com by reading the token from a file
+gh auth login --with-token < token.txt
+
+
+# Set the repository
+git clone https://github.com/IMYdev/TG-X-GH-actions_TWRP-Builder
+cd TG-X-GH-actions_TWRP-Builder
+gh repo set-default https://github.com/IMYdev/TG-X-GH-actions_TWRP-Builder
+
+# Pass everything as an argument coz why not
+Manifest_URL=$1
+Manifest_Branch=$2
+Device_Tree_URL=$3
+Device_Tree_Branch=$4
+Device_Path=$5
+Device_Name=$6
+Makefile_Name=$7
+Build_Target=$8
+
+# Run the GitHub Actions workflow with the specified URL
+gh workflow run build_recovery.yml -f MANIFEST_URL=$Manifest_URL -f MANIFEST_BRANCH=$Manifest_Branch -f DEVICE_TREE_URL=$Device_Tree_URL -f DEVICE_TREE_BRANCH=$Device_Tree_Branch -f DEVICE_PATH=$Device_Path -f DEVICE_NAME=$Device_Name -f MAKEFILE_NAME=$Makefile_Name -f BUILD_TARGET=$Build_Target
